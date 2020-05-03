6452f77dad0a312b631fed3f17b3c48a06e1eb19
nyabuto <nyabutovice@gmail.com>
2020-05-03 11:28:06 +0300

    create a new activity

---
+++ app/src/main/java/com/nyabuto/mybakery/BakeriesActivity.java
@@ -0,0 +1,14 @@
+package com.nyabuto.mybakery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BakeriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bakeries);
+    }
+}
--- app/src/main/java/com/nyabuto/mybakery/MainActivity.java
+++ app/src/main/java/com/nyabuto/mybakery/MainActivity.java
@@ -2,13 +2,28 @@ package com.nyabuto.mybakery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mFindBakeriesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindBakeriesButton = (Button)findViewById(R.id.findBakeriesButton);
+        mFindBakeriesButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BakeriesActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
